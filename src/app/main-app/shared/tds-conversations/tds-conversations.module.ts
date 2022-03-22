ffaebb4f33d4739320b0a071fb5b325f50187139
phuongnguyencntt <nguyenthanhphuongcntt@gmail.com>
2022-03-22 10:33:31 +0700

    cập nhật icon 1.5.1

--- src/app/main-app/shared/tds-conversations/tds-conversations.module.ts
+++ src/app/main-app/shared/tds-conversations/tds-conversations.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { FormsModule } from '@angular/forms';
-import { TDSOutletModule, TDSAvatarModule, TDSButtonModule} from 'tmt-tang-ui';
+import { TDSOutletModule, TDSAvatarModule, TDSButtonModule, TDSToolTipModule} from 'tmt-tang-ui';
 import { TDSConversationsComponent } from './tds-conversations.component';
 import { TDSConversationItemComponent } from './tds-conversation-item';
 
@@ -23,6 +23,7 @@ const CMP =[
     TDSOutletModule,
     TDSAvatarModule,
     TDSButtonModule,
+    TDSToolTipModule
   ],
   exports:[
     ...CMP
