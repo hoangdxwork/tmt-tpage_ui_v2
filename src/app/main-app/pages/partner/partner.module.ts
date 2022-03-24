242f31a73f6c340fa47ed74674836ea9047b6ec9
MinhHung <minhhungak97@gmail.com>
2022-03-24 17:24:51 +0700

    Fix icon, thêm components Partner

--- src/app/main-app/pages/partner/partner.module.ts
+++ src/app/main-app/pages/partner/partner.module.ts
@@ -1,3 +1,14 @@
+import { DrawerMessageComponent } from './components/drawer-message/drawer-message.component';
+import { ModalPaymentComponent } from './components/modal-payment/modal-payment.component';
+import { ModalBirthdayPartnerComponent } from './components/modal-birthday-partner/modal-birthday-partner.component';
+import { ModalSampleMessageComponent } from './components/modal-sample-message/modal-sample-message.component';
+import { ModalSendMessageComponent } from './components/modal-send-message/modal-send-message.component';
+import { ModalConvertPartnerComponent } from './components/modal-convert-partner/modal-convert-partner.component';
+import { ModalResetPointComponent } from './components/modal-reset-point/modal-reset-point.component';
+import { ModalEditPartnerComponent } from './components/modal-edit-partner/modal-edit-partner.component';
+import { ModalAddAddressComponent } from './components/modal-add-address/modal-add-address.component';
+import { ModalAddPartnerComponent } from './components/modal-add-partner/modal-add-partner.component';
+import { InfoOrderDebtOfPartnerComponent } from './components/info-order-debt-of-partner/info-order-debt-of-partner.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -5,17 +16,7 @@ import { CommonModule } from '@angular/common';
 import { PartnerRoutingModule } from './partner-routing.module';
 import { PartnerComponent } from './partner/partner.component';
 import { TDSButtonModule, TDSTabsModule, TDSModalService, TDSFormFieldModule, TDSInputModule, TDSDropDownModule, TDSTableModule, TDSTagModule, TDSAvatarModule, TDSBadgeModule, TDSPopoverModule, TDSSkeletonModule, TDSSelectModule, TDSToolTipModule, TDSModalModule, TDSRadioModule, TDSDatePickerModule, TDSCheckBoxModule, TDSInputNumberModule, TDSButtonSpitModule, TDSDrawerModule } from 'tmt-tang-ui';
-import { InfoOrderDebtOfPartnerComponent } from './partner/info-order-debt-of-partner/info-order-debt-of-partner.component';
-import { ModalAddPartnerComponent } from './partner/modal-add-partner/modal-add-partner.component';
-import { ModalAddAddressComponent } from './partner/modal-add-address/modal-add-address.component';
-import { ModalEditPartnerComponent } from './partner/modal-edit-partner/modal-edit-partner.component';
-import { ModalResetPointComponent } from './partner/modal-reset-point/modal-reset-point.component';
-import { ModalConvertPartnerComponent } from './partner/modal-convert-partner/modal-convert-partner.component';
-import { ModalSendMessageComponent } from './partner/modal-send-message/modal-send-message.component';
-import { ModalSampleMessageComponent } from './partner/modal-send-message/modal-sample-message/modal-sample-message.component';
-import { ModalBirthdayPartnerComponent } from './partner/modal-birthday-partner/modal-birthday-partner.component';
-import { ModalPaymentComponent } from './partner/modal-payment/modal-payment.component';
-import { DrawerMessageComponent } from './partner/drawer-message/drawer-message.component';
+
 
 
 @NgModule({
