4b737c7e197d2709069c557022a30eaea9247402
phuongnguyencntt <nguyenthanhphuongcntt@gmail.com>
2022-05-05 14:24:22 +0700

    fix lỗi change team, update icon 1.8.0

--- src/app/main-app/shared/tpage-base/tpage-base.component.ts
+++ src/app/main-app/shared/tpage-base/tpage-base.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { combineLatest, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-import { TDSHelperObject, TDSSafeAny } from 'tmt-tang-ui';
+import { combineLatest, of, Subject } from 'rxjs';
+import { map, mergeMap, takeUntil, tap, filter } from 'rxjs/operators';
+import { TDSHelperArray, TDSHelperObject, TDSSafeAny } from 'tmt-tang-ui';
 import { CRMTeamDTO } from '../../dto/team/team.dto';
 import { CRMTeamService } from '../../services/crm-team.service';
 import { TPageHelperService } from '../../services/helper.service';
@@ -19,42 +19,42 @@ export class TpageBaseComponent implements OnInit, OnDestroy {
   private _destroy = new Subject<void>();
   private _params!: TDSSafeAny;
   private _currentTeam!: CRMTeamDTO | null;
-
+  private _listFaceBook: Array<CRMTeamDTO> = [];
   constructor(public crmService: CRMTeamService,
-      public activatedRoute: ActivatedRoute,
-      public router: Router) { }
+    public activatedRoute: ActivatedRoute,
+    public router: Router) { }
 
   ngOnInit(): void {
-    combineLatest([this.activatedRoute.queryParams,
-      this.crmService.onChangeTeam(),
-      this.crmService.onChangeListFaceBook()])
-          .pipe(takeUntil(this._destroy))
-          .subscribe(([params, team, listTeam]) => {
-              this._params = params;
-              this._currentTeam = team;
-              if ((!TDSHelperObject.hasValue(params) || !TDSHelperObject.hasValue(params.teamId))) {
-                  if (!TDSHelperObject.hasValue(team)) {
-                    this.onRedirect();
-                  } else {
-                    let url = this.router.url.split("?")[0];
-                    if (this.paramsUrl.psid) {
-                      this.router.navigateByUrl(`${url}?teamId=${this.currentTeam?.Id}&type=${this.type}&psid=${this.paramsUrl.psid}`);
-                    } else {
-                      this.router.navigateByUrl(`${url}?teamId=${this.currentTeam?.Id}&type=${this.type}`);
-                    }
-                  }
-              } else {
-                  if (!TDSHelperObject.hasValue(team) || (team?.Id != params.teamId)) {
-                    const team = TPageHelperService.findTeamById(listTeam?.Items || [], params.teamId, false);
-                    if (team)
-                      this.crmService.onUpdateTeam(team);
-                    else
-                      this.onRedirect();
-                  } else {
-                    this.onInit();
-                  }
-            }
-      })
+    let that = this;
+    /**
+     * chỉ load lại data khi url thay đổi
+     */
+    this.activatedRoute.queryParamMap.pipe(
+      tap((queryParamMap: TDSSafeAny) => {
+        that._params = { ...queryParamMap?.params };
+      }),
+      mergeMap((params: TDSSafeAny) => {
+        return that.crmService.onChangeListFaceBook().pipe(
+          tap((listFb) => {
+            that._listFaceBook = listFb?.Items || [];
+          }),
+          map((listTeam) => {
+            const team = that.findTeamById(listTeam?.Items || [], that.paramsUrl?.teamId, false);
+            return team;
+          })
+        )
+      }),
+      takeUntil(this._destroy)
+    ).subscribe(
+      team => {      
+        if (!TDSHelperObject.hasValue(team)) {
+          this.onRedirect();
+        } else {
+          this._currentTeam = team;
+          this.onInit();
+        }
+      }
+    );
   }
 
   get paramsUrl() {
@@ -81,20 +81,46 @@ export class TpageBaseComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-      this._destroy.next();
-      this._destroy.complete();
-      this.onDestroy();
+    this._destroy.next();
+    this._destroy.complete();
+    this.onDestroy();
   }
 
   addQueryParams(queryParams: TDSSafeAny): any {
-    return  this.router.navigate([], {
-        relativeTo: this.activatedRoute,
-        queryParams: queryParams,
-        queryParamsHandling: 'merge',
-        // preserve the existing query params in the route
-        skipLocationChange: true
-        // do not trigger navigation
+    return this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams: queryParams,
+      queryParamsHandling: 'merge',
+      // preserve the existing query params in the route
+      skipLocationChange: true
+      // do not trigger navigation
     });
   }
-
+  findTeamById(dataTeam: Array<CRMTeamDTO>, teamId: TDSSafeAny, getFirstItem: boolean = false) {
+    let team: CRMTeamDTO | null = null;
+    if (!TDSHelperArray.hasListValue(dataTeam)) {
+      return team;
+    }
+    for (let index = 0; index < dataTeam?.length; index++) {
+      const item = dataTeam[index];
+      for (let index = 0; index < item.Childs.length; index++) {
+        const child = item.Childs[index];
+        if (teamId == child.Id) {
+          team = child;
+          break;
+        }
+      }
+      if (TDSHelperObject.hasValue(team)) {
+        break
+      }
+    }
+    if (!TDSHelperObject.hasValue(team) && getFirstItem) {
+      const firstItem = dataTeam.find(res => {
+        return res.Childs.length > 0
+      });
+      team = firstItem?.Childs[0] || null;
+    }
+    return team;
+  }
+ 
 }
