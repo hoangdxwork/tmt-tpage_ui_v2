56439424c8ef7d6a7f9b8f00064bfb51c0df0e13
toanle <toanlekhtn@gmail.com>
2022-08-01 11:53:21 +0700

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TDS_I18N, vi_VN } from 'tds-ui/i18n';
+import { LibModule } from '@core/lib.module';
 
 // Thiết lập tiếng Việt
 registerLocaleData(localeVi);
@@ -28,7 +29,8 @@ registerLocaleData(localeVi);
     AppRoutingModule,
     ScrollingModule,
     HttpClientModule,
-    SharedModule
+    SharedModule,
+    LibModule
   ],
   providers: [{ provide: TDS_I18N, useValue: vi_VN },
     TAuthGuardService, {
