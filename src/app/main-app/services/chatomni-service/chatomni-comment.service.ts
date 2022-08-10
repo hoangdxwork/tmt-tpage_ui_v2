9e2c167ca3a935971d365c28cee3492a67fd0434
toanle <toanlekhtn@gmail.com>
2022-08-10 14:38:43 +0700

    .

--- src/app/main-app/services/chatomni-service/chatomni-comment.service.ts
+++ src/app/main-app/services/chatomni-service/chatomni-comment.service.ts
@@ -95,7 +95,12 @@ export class ChatomniCommentService extends BaseSevice  {
         let url = this.urlNext  as string;
         return this.getLink(url).pipe(map((res: ChatomniDataDto) => {
 
-            exist.Extras!.Objects = Object.assign({}, exist.Extras?.Objects, res.Extras?.Objects);
+            if(res.Extras?.Objects) {
+              exist.Extras = {
+                  Objects: Object.assign({}, exist.Extras?.Objects, res.Extras?.Objects)
+              }
+            }
+
             exist.Items = [ ...exist.Items, ...res.Items ];
             exist.Paging = { ...res.Paging };
 
