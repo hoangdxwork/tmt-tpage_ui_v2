b2da0c89ec30e10941587f2e4dbbc504cc490b90
TMT\hungtm <Minh Hng>
2022-09-09 11:37:36 +0700

    fix jsonpayload

--- src/app/main-app/shared/pipe/json-payload.pipe.ts
+++ src/app/main-app/shared/pipe/json-payload.pipe.ts
@@ -13,18 +13,17 @@ export class jsonPayloadPipe implements PipeTransform {
   }
 
   transform(json: any) : any {
-    if(typeof json === 'string') {
-      if (/^[\[|\{](\s|.*|\w)*[\]|\}]$/.test(json)) {
-        let model = JSON.parse(json) as MessagePayloadDto;
-
-        let order_url = model.attachment?.payload?.order_url;
+    if(this.tryParseJSONObject(json)) {
+      let payload = this.tryParseJSONObject(json) as MessagePayloadDto;
+      if(payload && payload.attachment){
+        let order_url = payload.attachment?.payload?.order_url;
         let check = order_url.lastIndexOf('/');
         if(check !== -1) {
           order_url = order_url.substring(check).replace('/', '');
         }
 
-        if(model.attachment && model.attachment.payload){
-          let message = `${model.attachment.payload.recipient_name} đã tạo đơn hàng <span class="font-semibold cursor-pointer payload" id="${order_url}">#${model.attachment.payload.order_number}</span>`
+        if(payload.attachment && payload.attachment.payload){
+          let message = `${payload.attachment.payload.recipient_name} đã tạo đơn hàng <span class="font-semibold cursor-pointer payload" id="${order_url}">#${payload.attachment.payload.order_number}</span>`
 
           return message
         }
@@ -33,6 +32,23 @@ export class jsonPayloadPipe implements PipeTransform {
 
     return json;
   }
+
+  tryParseJSONObject (jsonString: any){
+    try {
+        var o = JSON.parse(jsonString);
+
+        // Handle non-exception-throwing cases:
+        // Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
+        // but... JSON.parse(null) returns null, and typeof null === "object", 
+        // so we must check for that, too. Thankfully, null is falsey, so this suffices:
+        if (o && typeof o === "object") {
+            return o;
+        }
+    }
+    catch (e) { }
+
+    return false;
+};
 }
 
 
