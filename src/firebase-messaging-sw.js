506f455c30ecd28daf1e5f26803b1619d461ddfe
toanlekhtn@gmail.com <54982565+toanleTMT@users.noreply.github.com>
2022-11-05 18:18:40 +0700

    setup firebase

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -3,32 +3,53 @@ importScripts('https://www.gstatic.com/firebasejs/9.13.0/firebase-messaging-comp
 importScripts('https://www.gstatic.com/firebasejs/9.13.0/firebase-analytics-compat.js');
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAAnP3M-JqrBLr-AGoHTZ1Qtx7QR0MXGVQ",
-  authDomain: "tposmobile.firebaseapp.com",
-  databaseURL: "https://tposmobile.firebaseio.com",
-  projectId: "tposmobile",
-  storageBucket: "tposmobile.appspot.com",
-  messagingSenderId: "852209026200",
-  appId: "1:852209026200:web:ef6ad3d5d7573855a63ced",
-  measurementId: "G-XHX5BTL43H"
+  apiKey: "AIzaSyB13Zlrc54Xz2qZwBOsZN1cGwkWUV7RDiw",
+  authDomain: "propane-bebop-278706.firebaseapp.com",
+  projectId: "propane-bebop-278706",
+  storageBucket: "propane-bebop-278706.appspot.com",
+  messagingSenderId: "528073803166",
+  appId: "1:528073803166:web:ebd0d1f5d380cdd86c428f",
+  measurementId: "G-PMJ8HGCCT1"
 };
 
 const messaging = firebase.messaging();
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-messaging.setBackgroundMessageHandler(function (payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-
-  const notificationTitle = payload.data.title;
+messaging.onMessage(function(payload) {
+  console.log(payload);
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
-      body: payload.data.body,
-      icon: '/firebase-logo.png',
-      data: payload.data
+      body: payload.notification.body,
+      icon: payload.notification.icon,
   };
+  // console.log(notificationTitle,notificationOptions)
 
-  return self.registration.showNotification(notificationTitle, notificationOptions);
+  if (!("Notification" in window)) {
+      console.log("This browser does not support system notifications.");
+  } else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      var notification = new Notification(notificationTitle,notificationOptions);
+      notification.onclick = function(event) {
+          event.preventDefault();
+          window.open(payload.notification.click_action , '_blank');
+          notification.close();
+      }
+  }
 });
+
+// messaging.setBackgroundMessageHandler(function (payload) {
+//   console.log('[firebase-messaging-sw.js] Received background message ', payload);
+
+//   const notificationTitle = payload.data.title;
+//   const notificationOptions = {
+//       body: payload.data.body,
+//       icon: '/firebase-logo.png',
+//       data: payload.data
+//   };
+
+//   return self.registration.showNotification(notificationTitle, notificationOptions);
+// });
 
 self.addEventListener('notificationclick', function (event) {
     event.notification.close();
