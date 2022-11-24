0cae40e1f5271f65384443a44fcc7349bb91c12c
toanlekhtn@gmail.com <54982565+toanleTMT@users.noreply.github.com>
2022-11-24 13:50:58 +0700

    fix nhẹ

--- src/app/main-app/services/chatomni-service/chatomni-conversation.service.ts
+++ src/app/main-app/services/chatomni-service/chatomni-conversation.service.ts
@@ -163,19 +163,12 @@ export class ChatomniConversationService extends BaseSevice {
   }
 
   getInfo(teamId: number, csid: string): Observable<any> {
-
     let id = `${teamId}_${csid}`;
     let api: CoreAPIDTO = {
         url: `${this._BASE_URL}/${this.baseRestApi}/info/${id}`,
         method: CoreApiMethodType.get
     }
     return this.apiService.getData<any>(api, null);
-  }
-
-  syncConversationInfo(teamId: number, csid: string): Observable<any> {
-    return this.getInfo(teamId, csid).pipe(map((x: any) => {
-        return x;
-    }),shareReplay(1))
   }
 
 }
