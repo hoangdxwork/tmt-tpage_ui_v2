5861ad00d410d2f6029bdd8aa6574404e72367cb
toanlekhtn@gmail.com <54982565+toanleTMT@users.noreply.github.com>
2022-12-13 15:14:29 +0700

    click window firebase notification

--- src/assets/firebase/firebase-messaging-sw.js
+++ src/assets/firebase/firebase-messaging-sw.js
@@ -13,4 +13,32 @@ firebase.initializeApp({
   measurementId: "G-XHX5BTL43H"
 });
 
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  // fcp_options.link field from the FCM backend service goes there, but as the host differ, it not handled by Firebase JS Client sdk, so custom handling
+  if (event && event.notification && event.notification.data && event.notification.data.FCM_MSG && event.notification.data.FCM_MSG.notification) {
+
+      const data = event.notification.data.FCM_MSG;
+      const url = `https://${data.data.TenantId}/tpagev2/#/user/firebase-notification?id=${data.data.NotificationId}`;
+
+      event.waitUntil(
+          self.clients.matchAll({type: 'window'}).then( windowClients => {
+              // Check if there is already a window/tab open with the target URL
+              for (var i = 0; i < windowClients.length; i++) {
+                  var client = windowClients[i];
+                  // If so, just focus it.
+                  if (client.url === url && 'focus' in client) {
+                      return client.focus();
+                  }
+              }
+              // If not, then open the target URL in a new window/tab.
+              if (self.clients.openWindow) {
+                  console.log("open window")
+                  return self.clients.openWindow(url);
+              }
+          })
+      )
+  }
+}, false);
+
 const messaging = firebase.messaging();
