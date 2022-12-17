577abd7f88c3d3a23d12c6f1bd4db05abdacb311
TMT\tailh <huutaile190297@gmail.com>
2022-12-17 15:37:04 +0700

    update skeleton v1

--- src/app/main-app/shared/directives/directives.module.ts
+++ src/app/main-app/shared/directives/directives.module.ts
@@ -1,3 +1,8 @@
+import { TDSSpinnerModule } from 'tds-ui/progress-spinner';
+import { CsLoadingPostDirective } from './cs-loading-post.directive';
+import { CsLoadingObjectDirective } from './cs-loading-object.directive';
+import { CsLoadingPartnerDirective } from './cs-loading-partner.directive';
+import { TDSSkeletonModule } from 'tds-ui/skeleton';
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { PipeModule } from "../pipe/pipe.module";
@@ -8,12 +13,17 @@ import { YuriAvatarDirective } from "./yuri-avatar.directive";
 const DIRECTIVES = [
   YiAutoScrollDirective,
   YuriAvatarDirective,
-  ShowMoreDirective
+  ShowMoreDirective,
+  CsLoadingPartnerDirective,
+  CsLoadingObjectDirective,
+  CsLoadingPostDirective
 ]
 
 @NgModule({
   imports: [
     CommonModule,
+    TDSSkeletonModule,
+    TDSSpinnerModule,
     PipeModule
   ],
   exports: [...DIRECTIVES],
