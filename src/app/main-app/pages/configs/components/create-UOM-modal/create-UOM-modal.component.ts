d536d2ebce55d7f08475045fefa098d692b07813
TMT\tailh <huutaile190297@gmail.com>
2022-12-21 10:18:48 +0700

    fix modal product UOM Type

--- src/app/main-app/pages/configs/components/create-UOM-modal/create-UOM-modal.component.ts
+++ src/app/main-app/pages/configs/components/create-UOM-modal/create-UOM-modal.component.ts
@@ -1,46 +1,49 @@
 import { TDSDestroyService } from 'tds-ui/core/services';
-import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { ProductTemplateService } from '../../../../services/product-template.service';
-import { HttpClient } from '@angular/common/http';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
 import { TDSMessageService } from 'tds-ui/message';
 import { TDSModalRef } from 'tds-ui/modal';
-import { TDSSafeAny } from 'tds-ui/shared/utility';
 
 @Component({
     selector: 'create-uom-modal',
     templateUrl: './create-uom-modal.component.html',
     providers: [TDSDestroyService]
 })
-export class CreateUOMModalComponent implements OnInit, OnDestroy {
+export class CreateUOMModalComponent implements OnInit {
     @Input() type!: string;
 
-    addUOMForm!: FormGroup;
+    _form!: FormGroup;
     name!: string;
+    isLoading: boolean = false;
 
     constructor(
         private modal: TDSModalRef,
         private message: TDSMessageService,
         private productTemplate: ProductTemplateService,
-        private formBuilder: FormBuilder,
+        private fb: FormBuilder,
         private destroy$: TDSDestroyService
     ) {
         this.initForm();
     }
 
     ngOnInit(): void {
-        this.checkName()
-    }
-
-    ngOnDestroy(): void {
-        this.destroy$.next();
-        this.destroy$.complete();
+        switch(this.type) {
+            case 'producer':
+                this.name = 'nhà sản xuất';
+                break;
+            case 'importer':
+                this.name = 'nhà nhập khẩu';
+                break;
+            case 'distributor':
+                this.name = 'nhà phân phối';
+                break;
+        }
     }
 
     initForm() {
-        this.addUOMForm = this.formBuilder.group({
+        this._form = this.fb.group({
             Address: [null],
             Code: [null],
             Email: [null],
@@ -51,58 +54,34 @@ export class CreateUOMModalComponent implements OnInit, OnDestroy {
         });
     }
 
-    prepareModel() {
-        let formModel = this.addUOMForm.value;
-        let model = {
-            Address: formModel.Address,
-            Code: formModel.Code,
-            Email: formModel.Email,
-            Name: formModel.Name,
-            Note: formModel.Note,
-            Phone: formModel.Phone,
-            Type: this.type
-        };
-
-        return model;
-    }
-
-    onSubmit() {
-        if (!this.addUOMForm.invalid) {
-            if (this.type) {
-                let model = this.prepareModel();
-                this.productTemplate.insertPartnerExt(model).pipe(takeUntil(this.destroy$)).subscribe(
-                    (res: TDSSafeAny) => {
-                        this.message.success('Thêm thành công');
-                        this.modal.destroy(model);
-                        this.addUOMForm.reset();
-                    },
-                    err => {
-                        this.message.error(err.error.message ?? 'Thêm thất bại');
-                    }
-                )
-            } else {
-                this.message.error('Chưa xác định type');
-            }
-        }
-    }
-
-    cancel() {
+    onCancel() {
         this.modal.destroy(null);
     }
 
-    save() {
-        this.onSubmit();
-    }
+    onSave() {
+        if (!this._form.controls["Name"].value) {
+            this.message.error(`Vui lòng nhập tên ${this.name}`);
+            return;
+        }
 
-    checkName() {
-        if (this.type == 'producer') {
-            this.name = 'nhà sản xuất'
-        }
-        if (this.type == 'importer') {
-            this.name = 'nhà nhập khẩu'
-        }
-        if (this.type == 'distributor') {
-            this.name = 'nhà phần phối'
-        }
+        this.isLoading = true;
+        let model = this._form.value;
+        model.Type = this.type;
+
+        this.productTemplate.insertPartnerExt(model).pipe(takeUntil(this.destroy$)).subscribe({
+            next: (res: any) => {
+
+                if(!res) return;
+                delete res["@odata.context"];
+                this.isLoading = false;
+                
+                this.message.success('Thêm thành công');
+                this.modal.destroy(res);
+            },
+            error: (err) => {
+                this.isLoading = false;
+                this.message.error(err.error.message || 'Thêm thất bại');
+            }
+        })
     }
 }
