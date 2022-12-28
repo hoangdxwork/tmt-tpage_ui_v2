6ce99ab2647c22d3a14c984b0ca4b710edb7fbe9
toanlekhtn@gmail.com <54982565+toanleTMT@users.noreply.github.com>
2022-12-28 11:44:11 +0700

    .

--- src/app/main-app/services/facades/generate-tag-attributes.facade.ts
+++ src/app/main-app/services/facades/generate-tag-attributes.facade.ts
@@ -30,7 +30,18 @@ export class GenerateTagAttributesFacade {
           let item1 = `${att}`;
           let item2 = `${att}`;
 
-          let valueAtt = attributeValues?.map((t: any) => TDSHelperString.stripSpecialChars(t.Value).toLocaleLowerCase().trim());
+          // let item3 = `${att}`;
+          // let item4 = `${att}`;
+
+          let valueAtt = attributeValues?.map((t: any) => TDSHelperString.stripSpecialChars(t.Value).toLocaleLowerCase().trim()) as any[];
+
+          for (let i = 0; i < valueAtt?.length; i++) {
+              item1 = `${item1}${valueAtt[i]}`;
+              item2 = `${item2} ${valueAtt[i]}`;
+          }
+
+          datas.push(item1);
+          datas.push(item2);
 
       })
 
