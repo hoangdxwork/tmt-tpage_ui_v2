cf12d9bca0d3e5db2eb7a7610ca25713a616ef88
TMT\tailh <huutaile190297@gmail.com>
2023-01-03 17:11:49 +0700

    fix api tiktok check

--- src/app/main-app/services/tiktok-service/tiktok.service.ts
+++ src/app/main-app/services/tiktok-service/tiktok.service.ts
@@ -11,66 +11,66 @@ import { BaseSevice } from '../base.service';
 })
 
 export class TiktokService extends BaseSevice {
-    prefix: string = "odata";
-    table: string = "";
-    baseRestApi: string = "";
+  prefix: string = "odata";
+  table: string = "";
+  baseRestApi: string = "";
 
-    private readonly cacheLoginUser = '_cache_login_user';
-    
-    constructor(private apiService: TCommonService,
-        private message: TDSMessageService) {
-        super(apiService)
-    }
+  private readonly cacheLoginUser = '_cache_login_user';
 
-    login(id: string, sessionId: string) {
-        const api: CoreAPIDTO = {
-            url: `${this._BASE_URL}/rest/v2.0/chatomni/unofficialtiktokcheck/${id}?sessionid=${sessionId}`,
-            method: CoreApiMethodType.get,
-        }
-      
-        return this.apiService.getData<any>(api, null);
-    }
-
-    refreshListen(id: string) {
-      const api: CoreAPIDTO = {
-          url: `${this._BASE_URL}/rest/v2.0/chatomni/unofficialtiktok/${id}`,
-          method: CoreApiMethodType.post,
-        }
-    
-        return this.apiService.getData<any>(api, null);
+  constructor(private apiService: TCommonService,
+    private message: TDSMessageService) {
+    super(apiService)
   }
 
-    refreshChannelToken(id: any, channelId: any, accessToken: any): Observable<any> {
-      const api: CoreAPIDTO = {
-        url: `${this._BASE_URL}/rest/v2.0/chatomni/${id}/refreshchanneltoken?id=${id}&channelId=${channelId}&accessToken=${accessToken}`,
-        method: CoreApiMethodType.post,
-      }
-  
-      return this.apiService.getData<any>(api, null);
+  login(id: string, sessionId: string) {
+    const api: CoreAPIDTO = {
+      url: `${this._BASE_URL}/rest/v2.0/chatomni/unofficialtiktokcheck?sessionId=${sessionId}&id=${id}`,
+      method: CoreApiMethodType.get,
     }
 
-    setCacheLoginUser(cacheData: TiktokUserDto) {
-      let model = {
-        data: cacheData,
-        type: CRMTeamType._UnofficialTikTok
-      }
-  
-      let data = JSON.stringify(model);
-      localStorage.setItem(this.cacheLoginUser, data);
+    return this.apiService.getData<any>(api, null);
+  }
+
+  refreshListen(id: string) {
+    const api: CoreAPIDTO = {
+      url: `${this._BASE_URL}/rest/v2.0/chatomni/unofficialtiktok/${id}`,
+      method: CoreApiMethodType.post,
     }
-  
-    getCacheLoginUser(): TiktokUserDto | null {
-      let data = localStorage.getItem(this.cacheLoginUser);
-  
-      if(data) {
-        let model = JSON.parse(data);
-        return model;
-      } else {
-        return null;
-      }
+
+    return this.apiService.getData<any>(api, null);
+  }
+
+  refreshChannelToken(id: any, channelId: any, accessToken: any): Observable<any> {
+    const api: CoreAPIDTO = {
+      url: `${this._BASE_URL}/rest/v2.0/chatomni/${id}/refreshchanneltoken?id=${id}&channelId=${channelId}&accessToken=${accessToken}`,
+      method: CoreApiMethodType.post,
     }
-  
-    removeCacheLoginUser() {
-      localStorage.removeItem(this.cacheLoginUser);
+
+    return this.apiService.getData<any>(api, null);
+  }
+
+  setCacheLoginUser(cacheData: TiktokUserDto) {
+    let model = {
+      data: cacheData,
+      type: CRMTeamType._UnofficialTikTok
     }
+
+    let data = JSON.stringify(model);
+    localStorage.setItem(this.cacheLoginUser, data);
+  }
+
+  getCacheLoginUser(): TiktokUserDto | null {
+    let data = localStorage.getItem(this.cacheLoginUser);
+
+    if (data) {
+      let model = JSON.parse(data);
+      return model;
+    } else {
+      return null;
+    }
+  }
+
+  removeCacheLoginUser() {
+    localStorage.removeItem(this.cacheLoginUser);
+  }
 }
