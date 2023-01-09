1280addf1aa6dfca8967a715234fc948cc80bdab
NHU HUY <nhuhuy0812@gmail.com>
2023-01-09 17:56:01 +0700

    fix attackment and lstImage

--- src/app/main-app/shared/pipe/get-image-livecampaign.pipe.ts
+++ src/app/main-app/shared/pipe/get-image-livecampaign.pipe.ts
@@ -1,17 +1,19 @@
 import { Pipe, PipeTransform } from "@angular/core";
-import { LiveCampaignService } from "@app/services/live-campaign.service";
 
 @Pipe({
   name: 'getImageLiveCampaign'
 })
 export class GetImageLiveCampaignPipe implements PipeTransform {
 
-  transform(value: any, lstImage: any): any {
+  transform(value: any, lstImage: {[key: string]: string}): string {
     if(value && value.ImageUrl) {
       return value.ImageUrl
     } else {
-      return lstImage[value?.ProductId]
+      if(lstImage && lstImage[value?.ProductId]) {
+        return lstImage[value?.ProductId]
+      }
     }
+    return '';
   }
 }
 
