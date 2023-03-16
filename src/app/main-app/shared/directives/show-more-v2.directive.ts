34bb488e546e7c5031fde4da2038ec420ff6cfa1
NHU HUY <nhuhuy0812@gmail.com>
2023-03-16 10:46:10 +0700

    show more text directive

--- src/app/main-app/shared/directives/show-more-v2.directive.ts
+++ src/app/main-app/shared/directives/show-more-v2.directive.ts
@@ -3,28 +3,25 @@ import { Input, Component, Output, EventEmitter, ViewChild, ElementRef, OnInit }
 
 
 @Component({
-  selector: '[showMore]',
+  selector: '[showMoreV2]',
   template: `<div [ngClass]="isMessageItem? '': 'whitespace-normal'">
-                <span class="mr-1" [ngClass]="this.isShowmore ? '' : 'see-more'" [innerHTML]="this.isShowmore ? (text | formatIconLike | bbcodeConvert ) : (text | formatIconLike | bbcodeConvert | slice:0:length )"></span>
-                <a class="text-accent-9 cursor-pointer cs-more" *ngIf="!isShowmore && text && text.length > length" (click)="showContent($event)">Xem thêm</a>
-                <a *ngIf="isShowmore" class="text-accent-9 cursor-pointer" (click)="showContent($event)">Thu gọn</a>
+              <span class="mr-1" [ngClass]="this.isShowmore ? '' : 'see-more whitespace-normal'" [innerHTML]="this.isShowmore ? (text | formatIconLike | bbcodeConvert ) : (text | formatIconLike | bbcodeConvert | slice:0:length )"></span>
+              <a class="text-accent-9 cursor-pointer cs-more" *ngIf="!isShowmore && text && text.length > length" (click)="showContent($event)">Xem thêm</a>
+              <a *ngIf="isShowmore" class="text-accent-9 cursor-pointer" (click)="showContent($event)">Thu gọn</a>
             </div>`,
 })
-export class ShowMoreDirective implements OnInit, OnChanges {
+export class ShowMoreDirectiveV2 implements OnInit, OnChanges {
     isInnerHTML: boolean = false;
     countChar = 74; //ký tự innerHTML;
-    countCharPhoneFomat = 62; //ký tự 'format type='text-copyable' value=' phone socket trả về
+    countCharPhoneFomat = 50; //ký tự 'format type='text-copyable' value=' phone socket trả về
     @Input()
     text!: string;
-
-    @Input()
-    isMessageItem!: boolean;
 
     @Input()
     length!: number;
 
     @Input()
-    searchText!: string;
+    isMessageItem!: boolean;
 
     @Output() dathangEvent: EventEmitter<any> = new EventEmitter<any>();
 
@@ -44,8 +41,8 @@ export class ShowMoreDirective implements OnInit, OnChanges {
 
     ngOnInit(): void {
       if(this.text){
-          this.isInnerHTML = this.text.includes('<span ' && '</span>');
-          this.length = this.isInnerHTML ? (this.length + this.countChar) : this.length;
+        this.isInnerHTML = this.text.includes('<span ' && '</span>');
+        this.length = this.isInnerHTML ? (this.length + this.countChar) : this.length;
       }
     }
 
